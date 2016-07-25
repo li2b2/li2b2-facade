281e36f3da9abb8f0b35f4f05027de940aaf8442
R.W.Majeed <raphael@majeed.de>
2016-07-25 19:54:02 +0200

    common message code moved up to HiveMessage from Response

--- li2b2-client/src/main/java/de/sekmi/li2b2/client/HiveMessage.java
+++ li2b2-client/src/main/java/de/sekmi/li2b2/client/HiveMessage.java
@@ -3,6 +3,7 @@ package de.sekmi.li2b2.client;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public abstract class HiveMessage {
 	Document dom;
@@ -26,5 +27,51 @@ public abstract class HiveMessage {
 			return null;
 		}
 	}
+	/**
+	 * Get the required body content element with the specified namespace and qualified name.
+	 * 
+	 * @param namespace element's namespace
+	 * @param qualifiedName element's qualified name
+	 * @return DOM element node
+	 * @throws HiveException required node not found in message body
+	 */
+	public Element requireBodyElement(String namespace, String qualifiedName) throws HiveException{
+		Element body = getMessageBody();
+		Node n = body.getFirstChild();
+		if( n == null || n.getNodeType() != Node.ELEMENT_NODE 
+				|| n.getNamespaceURI() == null || !n.getNamespaceURI().equals(namespace)
+				|| !n.getLocalName().equals(qualifiedName) )
+		{
+			throw new HiveException("required element [ns="+namespace+", qn="+qualifiedName+"] not found in message body. Instead got "+n);
+		}
+		return (Element)n;
+	}
 
+	public HiveMessage setSecurity(Credentials credentials){
+		Element mh = getMessageHeader();
+		NodeList nl = mh.getElementsByTagName("security").item(0).getChildNodes();
+		nl.item(0).setTextContent(credentials.getDomain());
+		nl.item(1).setTextContent(credentials.getUser());
+		Element e = (Element)nl.item(2);
+		if( credentials.isToken() ){
+			e.setAttribute("is_token", "true");
+		}else{
+			e.removeAttribute("is_token");
+		}
+		e.setTextContent(credentials.getPassword());
+		return this;
+	}
+	
+	public HiveMessage setProjectId(String projectId){
+		Element mh = getMessageHeader();
+		mh.getElementsByTagName("project_id").item(0).setTextContent(projectId);
+		return this;
+	}
+	public HiveMessage setMessageId(String id, String inst){
+		Element mh = getMessageHeader();
+		NodeList nl = mh.getElementsByTagName("message_control_id").item(0).getChildNodes();
+		nl.item(0).setTextContent(id);
+		nl.item(1).setTextContent(inst);
+		return this;
+	}
 }
--- li2b2-client/src/main/java/de/sekmi/li2b2/client/Response.java
+++ li2b2-client/src/main/java/de/sekmi/li2b2/client/Response.java
@@ -45,24 +45,4 @@ public class Response extends HiveMessage{
 			throw new RuntimeException("No message body in response:"+n);
 		}
 	}
-
-	/**
-	 * Get the required body content element with the specified namespace and qualified name.
-	 * 
-	 * @param namespace element's namespace
-	 * @param qualifiedName element's qualified name
-	 * @return DOM element node
-	 * @throws HiveException required node not found in message body
-	 */
-	public Element requireBodyElement(String namespace, String qualifiedName) throws HiveException{
-		Element body = getMessageBody();
-		Node n = body.getFirstChild();
-		if( n == null || n.getNodeType() != Node.ELEMENT_NODE 
-				|| n.getNamespaceURI() == null || !n.getNamespaceURI().equals(namespace)
-				|| !n.getLocalName().equals(qualifiedName) )
-		{
-			throw new HiveException("required element [ns="+namespace+", qn="+qualifiedName+"] not found in message body. Instead got "+n);
-		}
-		return (Element)n;
-	}
 }
