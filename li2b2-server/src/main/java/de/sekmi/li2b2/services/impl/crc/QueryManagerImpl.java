63124f99d759ae6255524b0b3b5b2ebfca54a837
R.W.Majeed <raphael@majeed.de>
2016-08-04 13:33:45 +0200

    simple crc implementation

--- li2b2-server/src/main/java/de/sekmi/li2b2/services/impl/crc/QueryManagerImpl.java
+++ li2b2-server/src/main/java/de/sekmi/li2b2/services/impl/crc/QueryManagerImpl.java
@@ -1,37 +1,44 @@
 package de.sekmi.li2b2.services.impl.crc;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.w3c.dom.Element;
 
 import de.sekmi.li2b2.api.crc.QueryManager;
-import de.sekmi.li2b2.api.crc.QueryMaster;
+import de.sekmi.li2b2.api.crc.Query;
 import de.sekmi.li2b2.api.crc.ResultType;
 
 public class QueryManagerImpl implements QueryManager{
-
+	List<ResultTypeImpl> types;
+	
+	public QueryManagerImpl(){
+		types = new ArrayList<>(5);
+	}
+	public void addResultType(String name, String displayType, String description){
+		types.add(new ResultTypeImpl(name, displayType, description));
+	}
 	@Override
-	public QueryMaster runQuery(Element definition, List<ResultType> results) {
+	public Query runQuery(Element definition, List<ResultType> results) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public QueryMaster getQuery(String queryId) {
+	public Query getQuery(String queryId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Iterable<QueryMaster> listQueries(String userId) {
-		// TODO Auto-generated method stub
-		return null;
+	public Iterable<Query> listQueries(String userId) {
+		return Collections.emptyList();
 	}
 
 	@Override
-	public Iterable<ResultType> getResultTypes() {
-		// TODO Auto-generated method stub
-		return null;
+	public Iterable<? extends ResultType> getResultTypes() {
+		return types;
 	}
 
 }
