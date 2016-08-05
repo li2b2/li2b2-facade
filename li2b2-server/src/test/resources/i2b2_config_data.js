4f9ba25551a27e7ede6cf63caa593c93bf7af79a
R.W.Majeed <raphael@majeed.de>
2016-08-05 11:29:12 +0200

    fixed webclient config domain name

--- li2b2-server/src/test/resources/i2b2_config_data.js
+++ li2b2-server/src/test/resources/i2b2_config_data.js
@@ -5,7 +5,7 @@
 	// THESE ARE ALL THE DOMAINS A USER CAN LOGIN TO
 	lstDomains: [
 		{ domain: "i2b2demo",
-		  name: "HIStream test emulator",
+		  name: "li2b2 Demo Server",
 		  urlCellPM: "/i2b2/services/PMService/",
 		  allowAnalysis: true,
 		  installer: "/webclient/plugin_installer/",
