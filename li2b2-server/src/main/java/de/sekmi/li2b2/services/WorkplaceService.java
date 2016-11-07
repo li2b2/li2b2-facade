105151bc9308367e8b98b04ce8bf3aae5b3113b6
R.W.Majeed <raphael@majeed.de>
2016-11-07 22:14:44 +0100

    cleanup imports

--- li2b2-server/src/main/java/de/sekmi/li2b2/services/WorkplaceService.java
+++ li2b2-server/src/main/java/de/sekmi/li2b2/services/WorkplaceService.java
@@ -11,7 +11,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.dom.DOMSource;
 
 import org.w3c.dom.Element;
 
