7d7f62879756a00b784f178113aeb9e2ad788303
R.W.Majeed <raphael@majeed.de>
2018-10-10 15:55:27 +0200

    static method for unmarshalling

--- xml/src/main/java/de/sekmi/li2b2/hive/pm/Param.java
+++ xml/src/main/java/de/sekmi/li2b2/hive/pm/Param.java
@@ -1,8 +1,20 @@
 package de.sekmi.li2b2.hive.pm;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlValue;
+import javax.xml.transform.dom.DOMSource;
 
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+@XmlRootElement(name="param")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Param {
 	protected Param(){
 	}
@@ -24,4 +36,20 @@ public class Param {
 	public String name;
 	@XmlValue
 	public String value;
+
+	/**
+	 * Parse a list of param elements.
+	 * @param paramElements node list containing only param elements
+	 * @return unmarshalled param array
+	 */
+	public static Param[] parseNodeList(NodeList paramElements) throws JAXBException{
+		Param [] params  = new Param[paramElements.getLength()];
+		Unmarshaller um = JAXBContext.newInstance(Param.class).createUnmarshaller();
+		for (int i = 0; i < params.length; i ++) {
+			Element user = (Element)paramElements.item(i);
+			params[i] = (Param) um.unmarshal(new DOMSource(user));
+		}
+		return params;
+		
+	}
 }
