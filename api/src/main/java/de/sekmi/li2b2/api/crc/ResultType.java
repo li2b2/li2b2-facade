15ccdca71b49c2d8efeda2e0f1f5713712fbb3b0
R.W. Majeed <rwmajeed@gmail.com>
2020-02-24 03:10:47 +0100

    added more result types

--- api/src/main/java/de/sekmi/li2b2/api/crc/ResultType.java
+++ api/src/main/java/de/sekmi/li2b2/api/crc/ResultType.java
@@ -5,6 +5,8 @@ public interface ResultType {
 	public static final ResultType PATIENT_GENDER_COUNT_XML = new ResultTypeCategorial("PATIENT_GENDER_COUNT_XML", "Gender patient breakdown");
 	public static final ResultType PATIENT_VITALSTATUS_COUNT_XML = new ResultTypeCategorial("PATIENT_VITALSTATUS_COUNT_XML", "Vital Status patient breakdown");
 	public static final ResultType PATIENT_AGE_COUNT_XML = new ResultTypeCategorial("PATIENT_AGE_COUNT_XML", "Age patient breakdown");
+	public static final ResultType PATIENT_RACE_COUNT_XML = new ResultTypeCategorial("PATIENT_RACE_COUNT_XML", "Race patient breakdown");
+	public static final ResultType PATIENT_INOUT_XML = new ResultTypeCategorial("PATIENT_INOUT_XML", "Inpatient and outpatient breakdown");
 
 	String getName();
 	String getDisplayType(); // allowed: LIST, CATNUM
